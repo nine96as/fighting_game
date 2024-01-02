222fa1cd86c5a69faa91f29fe16fc1176ffd9aae
nine96as <nine96@skiff.com>
2024-01-02 18:39:37 +0000

    Update player class with selected_char attr

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 class Player:
-    def __init__(self, name, points):
+    def __init__(self, name, selected_character, points=0):
         self.name = name
+        self.selected_character = selected_character
         self.points = points
